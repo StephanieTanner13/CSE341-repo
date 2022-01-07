c4a6ca4a33849be3f94f10d77996dba494221030
StephanieTanner13 <80174471+StephanieTanner13@users.noreply.github.com>
2022-01-07 15:55:19 -0700

    Add files via upload

--- routes/ta02.js
+++ routes/ta02.js
@@ -3,12 +3,33 @@
 const express = require('express');
 const router = express.Router();
 
+const usernames = [];
+
+//requirement 2
+router.post('/addUser', (req, res, next) =>{
+  const newUser = req.body.newUser;
+
+  usernames.push(newUser);
+
+  res.redirect('/ta02/');
+});
+
+//requirement 3
+router.post('/removeUser', (req, res, next) =>{
+  const removeUser = req.body.removeUser;
+
+  const list = usernames.indexOf(removeUser);
+  if(list > -1) {
+    usernames.splice(list, 1);
+  }
+  res.redirect('/ta02/');
+});
+
 router.get('/', (req, res, next) => {
-  res.render('pages', {
+  res.render('pages/ta02', {
     title: 'Team Activity 02',
-    path: '/ta02', // For pug, EJS
-    activeTA03: true, // For HBS
-    contentCSS: true, // For HBS
+    path: '/ta02', //ejs
+    users: usernames
   });
 });
 
