656b3ade8e734bad2e21a568982ee83f0d491a95
StephanieTanner13 <80174471+StephanieTanner13@users.noreply.github.com>
2022-01-12 09:06:20 -0700

    Add files via upload

--- routes/ta03.js
+++ routes/ta03.js
@@ -1,14 +1,68 @@
 //TA03 PLACEHOLDER
 const express = require('express');
 const router = express.Router();
+const http = require('https');
+
+var url = 'https://byui-cse.github.io/cse341-course/lesson03/items.json';
+
+// // var options = {
+// //     host: 'www.random.org',
+// //     path: '/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new',
+// // };
+// }
+
+function addAllTags(data) {
+    const set = new Set();
+    for (let item of data) {
+        for (let tag of item.tags) {
+            set.add(tag);
+        }
+    }
+    return set;
+}
 
 router.get('/', (req, res, next) => {
-  res.render('pages/ta03', {
-    title: 'Team Activity 03',
-    path: '/ta03', // For pug, EJS
-    activeTA03: true, // For HBS
-    contentCSS: true, // For HBS
-  });
+    http.get(url, (responce) => {
+        let str = '';
+        responce.on('data', (chunk) => {
+            str += chunk;
+        });
+        responce.on('end', () => {
+            const data = JSON.parse(str);
+            const set = addAllTags(data);
+
+            const tag = req.query.tag;
+            const tags = req.query.tags
+                ? req.query.tags.replace(/\s/g, '')
+                : '';
+            const tagArray = tags === '' ? [] : tags.split(',');
+            let itemsToShow = data;
+            for (let tagItem of tagArray) {
+                itemsToShow = filterItems(itemsToShow, tagItem);
+            }
+
+            res.render('pages/ta03', {
+                title: 'Team Activity 03',
+                path: '/ta03', // For pug, EJS
+                activeTA03: true, // For HBS
+                contentCSS: true, // For HBS\
+                items: itemsToShow,
+                tags: set,
+            });
+        });
+        responce.on('error', (err) => {
+            console.log(err);
+        });
+    });
 });
+
+function filterItems(items, tag) {
+    const itemsToShow = items.filter((item) => {
+        return item.tags.find((itemTag) => {
+            return itemTag === tag;
+        });
+    });
+    return itemsToShow;
+}
 
 module.exports = router;
